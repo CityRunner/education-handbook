f601581ba7972513bc52b743a7822aeb18f0c66a
Dmitry Yudin <dima@localhost.localdomain>
2024-02-12 23:01:54 +0200

    Add tasks k-m

---
+++ python/unit_2/topic_2/task_k.py
@@ -0,0 +1,40 @@
+# Красота спасёт мир
+#
+# Одно из древних поверий гласит, что трёхзначное число красиво,
+# если сумма его минимальной и максимальной цифр
+# равна оставшейся цифре умноженной на 2.
+#
+# Напишите систему определяющую красоту числа.
+#
+# Формат ввода
+# Одно трёхзначное число
+#
+# Формат вывода
+# YES — если число красивое, иначе — NO
+
+number = int()
+digit_pos = []
+digit_sort = []
+
+while number not in range(100, 1000):
+    number = int(input())
+
+digit_pos.append(number // 1 % 10)
+digit_pos.append(number // 10 % 10)
+digit_pos.append(number // 100 % 10)
+
+while digit_pos and len(digit_sort) < 2:
+    digit_max = 0
+    digit_id = 0
+    for index in range(len(digit_pos)):
+        if digit_pos[index] >= digit_max:
+            digit_max = digit_pos[index]
+            digit_id = index
+    digit_sort.append(digit_pos[digit_id])
+    digit_pos.pop(digit_id)
+digit_sort.append(digit_pos[0])
+
+if (digit_sort[0] + digit_sort[2]) == digit_sort[1] * 2:
+    print("YES")
+else:
+    print("NO")
