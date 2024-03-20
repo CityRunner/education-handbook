3f76237043b512a5d7d9cabf458c25ee90bfe1b4
Dmitry Yudin <48123036+CityRunner@users.noreply.github.com>
2024-03-20 14:18:48 +0200

    Add tasks m-o, corrected various comments

--- python/unit_2/topic_4/task_s.py
+++ python/unit_2/topic_4/task_s.py
@@ -11,13 +11,6 @@
 # Требуется вывести сформированный числовой квадрат требуемого размера.
 # Чтобы квадрат был красивым, каждый его столбец — одинаковой ширины.
 
-# for i in range(n := int(input())):
-#     for j in range(n):
-#         d = str(min(i, j, n - i - 1, n - j - 1) + 1)
-#         print(d.rjust(len(str((n + 1) // 2)), ' '),
-#               end=' ' if j < n - 1 else '\n')
-# print('=' * 35)
-
 
 for row in range(size := int(input())):
     string = ''
