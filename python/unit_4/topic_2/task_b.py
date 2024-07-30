633de878f6f335cf2c36726244c9c12a8f32e86d
Dmitry Y <bigcityrunner@gmail.com>
2024-07-30 09:28:04 +0300

    Add task a-d

---
+++ python/unit_4/topic_2/task_b.py
@@ -0,0 +1,26 @@
+# Генератор матриц
+
+# Напишите функцию make_matrix, которая создаёт, заполняет и
+# возвращает матрицу заданного размера.
+# Параметры функции:
+#     size — кортеж (ширина, высота) или одно число (для
+#     создания квадратной матрицы);
+#     value — значение элементов списка (по-умолчанию 0).
+
+# Примечание
+# Ваше решение должно содержать только функции.
+# В решении не должно быть вызовов требуемых функций.
+
+def make_matrix(size, value=0):
+    from itertools import repeat
+    if type(size).__name__ == 'int':
+        return [list(repeat(value, size)) for _ in range(size)]
+    else:
+        width, height = size
+        return [list(repeat(value, width)) for _ in range(height)]
+
+
+result = make_matrix(3)
+print(result)
+result = make_matrix((4, 2), 1)
+print(result)
