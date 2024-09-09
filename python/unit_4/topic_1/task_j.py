edc244d95adc71ed021ead09681d39fb9a4ad97d
Dmitry Y <bigcityrunner@gmail.com>
2024-09-09 19:54:49 +0300

    Add task e, refactor task 4-1-j

--- python/unit_4/topic_1/task_j.py
+++ python/unit_4/topic_1/task_j.py
@@ -9,13 +9,17 @@
 # В решении не должно быть вызовов требуемых функций.
 # В этой задаче отключены стандартные сортировки.
 
-def merge(first, second):
-    numbers = list(first + second)
-    sorted_numbers = []
-    while numbers:
-        sorted_numbers.append(min(numbers))
-        numbers.remove(min(numbers))
-    return tuple(sorted_numbers)
+def merge(tuple_1, tuple_2):
+    list_1 = list(tuple_1)
+    list_2 = list(tuple_2)
+    result = []
+    while list_1 and list_2:
+        if list_1[0] > list_2[0]:
+            result.append(list_2.pop(0))
+        else:
+            result.append(list_1.pop(0))
+    result.extend(list_1 + list_2)
+    return tuple(result)
 
 
 # result = merge((1, 2), (3, 4, 5))
