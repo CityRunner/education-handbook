72857567c75c2f69de3a84fd2e0cfa4c3922a5d7
Dmitry Y <bigcityrunner@gmail.com>
2024-09-09 22:55:27 +0300

    Add task f, refactor task 2-2-q

--- python/unit_2/topic_2/task_q.py
+++ python/unit_2/topic_2/task_q.py
@@ -24,8 +24,6 @@ if a == b == c == 0:
     print("Infinite solutions")
 elif a == b == 0:
     print("No solution")
-elif a == c == 0:
-    print(0, 0)
 elif a == 0 and b != 0 and c != 0:
     print(-(c / b))
 else:
--- python/unit_5/topic_3/task_f.py
+++ python/unit_5/topic_3/task_f.py
@@ -12,7 +12,6 @@
 # Если переданные коэффициенты не являются рациональными числами,
 # вызовите исключение TypeError.
 
-
 class NoSolutionsError(Exception):
     pass
 
@@ -23,14 +22,12 @@ class InfiniteSolutionsError(Exception):
 
 def find_roots(a, b, c):
     for x in (a, b, c):
-        if not isinstance(x, (int, float)) or isinstance(x, bool):
+        if not isinstance(x, (int, float, complex)) or isinstance(x, bool):
             raise TypeError
     if a == b == c == 0:
         raise InfiniteSolutionsError
     elif a == b == 0:
         raise NoSolutionsError
-    elif a == c == 0:
-        return (0, 0)
     elif a == 0 and b != 0 and c != 0:
         return (-(c / b), -(c / b))
     else:
@@ -38,9 +35,8 @@ def find_roots(a, b, c):
         if D == 0:
             return (-b / (2 * a), -b / (2 * a))
         elif D > 0:
-            x1 = (-b - D ** 0.5) / (2 * a)
-            x2 = (-b + D ** 0.5) / (2 * a)
+            x1 = (-b - (D ** 0.5)) / (2 * a)
+            x2 = (-b + (D ** 0.5)) / (2 * a)
             return tuple(sorted([x1, x2]))
         else:
             raise NoSolutionsError
-
