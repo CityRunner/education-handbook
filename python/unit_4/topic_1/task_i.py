611baeac40ac4c4c39b446f46380880bba04dbc9
Dmitry Y <bigcityrunner@gmail.com>
2024-10-04 18:08:51 +0300

    Refactor unit_4/topic_1

--- python/unit_4/topic_1/task_i.py
+++ python/unit_4/topic_1/task_i.py
@@ -1,23 +1,29 @@
-# Простая задача 5.0
+"""
+Простая задача 5.0
 
-# Напишите функцию is_prime, которая принимает натуральное
-# число, а возвращает булево значение: True — если переданное
-# число простое, а иначе — False.
+Напишите функцию is_prime, которая принимает натуральное число, а возвращает булево значение: True — если переданное число простое, а иначе — False.
 
-# Примечание
-# Ваше решение должно содержать только функции.
-# В решении не должно быть вызовов требуемых функций.
+Примечание
+    Ваше решение должно содержать только функции.
+    В решении не должно быть вызовов требуемых функций.
 
+Пример 1:
+    Ввод:
+        result = is_prime(1001459)
+    Вывод:
+        result = True
 
-def is_prime(input_num):
-    if (current_num := input_num) > 1:
-        for div in range(2, int(current_num ** 0.5) + 1):
-            if current_num % div == 0:
-                return False
-    else:
-        return False
+Пример 2:
+    Ввод:
+        result = is_prime(79701)
+    Вывод:
+        result = False
+"""
+
+def is_prime(num):
+    prime_state = num > 1
+    if prime_state:
+        for div in range(2, int(num ** 0.5) + 1):
+            if num % div == 0:
+                return = False
     return True
-
-
-# result = is_prime(1001459)
-# print(result)
